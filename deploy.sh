96c78eb11dbf29b0a07075c557fc55710d659a27
Reverseblade <yuta.fujisawa.1987@gmail.com>
2019-09-27 22:57:56 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ln -sf ~/dotfiles/config.fish ~/.config/fish 
-ln -sf ~/dotfiles/dot.bashrc ~/.bashrc
+# ln -sf ~/dotfiles/dot.bashrc ~/.bashrc
 ln -sf ~/dotfiles/dot.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/dot.vimrc ~/.vimrc
 ln -sf ~/dotfiles/dot.zshrc ~/.zshrc
